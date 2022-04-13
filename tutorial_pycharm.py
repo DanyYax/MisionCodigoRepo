88300b6e2fc3918fae41360062a0c352c7a94b6a
Dany Jacquez <dany_jac@misioncodigo.com>
2022-04-13 09:11:06 -0500

    Codigo usado en el Video tutorial de pycharm en Youtube https://youtu.be/-Dbpna_Y-dI

---
+++ tutorial_pycharm.py
@@ -0,0 +1,13 @@
+#
+# Escribir lo que queramos en nuestro programa
+#
+
+def sumar(a, b):
+    c = a + b
+    return c
+
+# ejemplo 1:
+j = 1
+for i in range(10):
+    resultado = sumar(i, j)
+    print(resultado)
